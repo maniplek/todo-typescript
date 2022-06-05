aded776a214fee72af46a1c81fd9283edd77024f
Maniple <maniplek81@gmail.com>
2022-06-05 23:21:30 +0200

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
+import TodoList from './component/TodoList';
 
-function App() {
+
+import { ITask } from './InterfaceTask';
+
+
+
+
+const App:FC = () =>{
+
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadLine] = useState<number>(1);
+  const [allTask, setAllTask] = useState<ITask[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void =>{
+    if(e.target.name ==='task'){
+      setTask(e.target.value);
+    }else{
+      setDeadLine(Number(e.target.value));
+    }
+  }
+
+
+  const addTask = ():void =>{
+    const newTask = {taskName: task, deadline: deadline}
+   setAllTask([...allTask, newTask]);
+  
+   setTask('');
+   setDeadLine(0);
+   console.log(allTask);
+  }
+
+  const completedTask = (taskToBeDeleted:string): void =>{
+    setAllTask(allTask.filter((task)=>(task.taskName !== taskToBeDeleted)));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container flex flex-col items-center  m-10 gap-9">
+        <h3 className='text-3xl text-green-500 font-bold '>TODO APP </h3>
+      <div className='flex gap-5'>
+        <input className='border-2 border-green-600' type='text' name='task' onChange={handleChange} value={task} placeholder='Enter your Task'/>
+        <input className='border-2 border-green-600' type='number' name='deadline' onChange={handleChange} value={deadline} placeholder='Enter your deadline'/>
+        <button className='border-2 border-green-600 bg-green-500 text-white' onClick={addTask}>Add Task</button>
+      </div>
+      <div >
+        <h3 className='text-xl font-semibold text-green-500'>TODO LIST</h3>
+        {
+          allTask.map((task: ITask, key:number)=>(
+            <TodoList key={key} task={task} completedTask={completedTask} />
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/component/TodoList.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ITask } from '../InterfaceTask'
+
+
+interface Props {
+    task:ITask;
+    completedTask(taskToBeDeleted:string):void;
+}
+
+const TodoList = ({task, completedTask}:Props) => {
+  return (
+    <div className='flex justify-around gap-6 '>
+ <div className='text-xl '>{task.taskName} will be done in {task.deadline} days
+ 
+    <button className='bg-red-200 p-2 border border-red-800 rounded' onClick={()=>{
+        completedTask(task.taskName)
+    }}>
+       <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+    </button>
+    </div>
+    </div>
+   
+  )
+}
+
+export default TodoList
